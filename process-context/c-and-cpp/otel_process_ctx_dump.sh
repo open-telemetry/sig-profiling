650085a93d976350d114ecb3ecd5a431b16fc7c7
Ivo Anjo <ivo.anjo@datadoghq.com>
2026-03-13 11:12:00 +0000

    Use `CLOCK_BOOTTIME` instead of `CLOCK_REALTIME` in `published_at_ns` in process context C/C++ impl (#79)
    **What does this PR do?**
    
    This PR updates the process context C/C++ implementation to match the
    change in the upstream specification in
    
    https://github.com/open-telemetry/opentelemetry-specification/commit/15256ee2b830498c2878f3d27b75b48d08dcfd23
    to adopt a monotonic clock.
    
    **Motivation:**
    
    Using a monotonic clock means that readers can't get confused by clocks
    going backward, and we can trivially about the ABA issue (because if the
    clock can't ever go back, we can always add 1 to a previous read to
    ensure no two timestamps are ever the same).
    
    **Additional Notes:**
    
    In
    https://github.com/open-telemetry/sig-profiling/pull/66#discussion_r2911913478
    we discussed that this could be implemented by directly using the
    `CLOCK_MONOTONIC`, or alternatively we could map the monotonic clock
    back to a regular ns from epoch.
    
    I had this commit already ready and it takes the first approach, but
    let's discuss? ;)
    
    **How to test the change?**
    
    In practice readers don't really need to change unless they were trying
    to interpret the timestamp and pretty-print it (as the bash script was).

--- process-context/c-and-cpp/otel_process_ctx_dump.sh
+++ process-context/c-and-cpp/otel_process_ctx_dump.sh
@@ -57,7 +57,7 @@ version="$(
 payload_size="$(
   printf '%s' "$data_b64" | base64 -d | dd bs=1 skip=12 count=4 status=none | od -An -t u4 | tr -d ' '
 )"
-published_at_ns="$(
+monotonic_published_at_ns="$(
   printf '%s' "$data_b64" | base64 -d | dd bs=1 skip=16 count=8 status=none | od -An -t u8 | tr -d ' '
 )"
 payload_ptr_hex="$(
@@ -65,14 +65,11 @@ payload_ptr_hex="$(
 )"
 
 echo "Parsed struct:"
-echo "  otel_process_ctx_signature       : \"$signature\""
-echo "  otel_process_ctx_version         : $version"
-# Convert nanoseconds to seconds for date command
-published_at_s=$((published_at_ns / 1000000000))
-published_at_pretty="$(date -d "@$published_at_s" '+%Y-%m-%d %H:%M:%S %Z')"
-echo "  otel_process_payload_size        : $payload_size"
-echo "  otel_process_ctx_published_at_ns : $published_at_ns ($published_at_pretty)"
-echo "  otel_process_payload             : 0x$payload_ptr_hex"
+echo "  otel_process_ctx_signature             : \"$signature\""
+echo "  otel_process_ctx_version               : $version"
+echo "  otel_process_payload_size              : $payload_size"
+echo "  otel_process_monotonic_published_at_ns : $monotonic_published_at_ns"
+echo "  otel_process_payload                   : 0x$payload_ptr_hex"
 
 echo "Payload dump ($payload_size bytes):"
 dd if="/proc/$pid/mem" bs=1 count="$payload_size" skip=$((16#$payload_ptr_hex)) status=none | hexdump -C
